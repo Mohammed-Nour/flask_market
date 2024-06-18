9e25f8fa799940edfa86dd43c9727d7dc2788eae
Mohamad Nour Shahin <o.shaheen1882@gmail.com>
2024-06-19 00:06:01 +0300

    finsih the whole course

---
+++ market/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import  SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'eb158b6a5d5de60301c31df0'
+db= SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+from market import routes
+
+
+# with app.app_context():
+#     db.create_all()
+
+
+
+
+
+
